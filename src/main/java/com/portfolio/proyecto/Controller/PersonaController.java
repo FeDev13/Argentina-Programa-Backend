1115de1e5d9ee21c72f30d06efb37849e5924b91
USUARIO <USUARIO@DESKTOP-P3D2CGR.fibertel.com.ar>
2022-07-22 13:34:22 -0300

    first commit

--- src/main/java/com/portfolio/proyecto/Controller/PersonaController.java
+++ src/main/java/com/portfolio/proyecto/Controller/PersonaController.java
@@ -5,6 +5,7 @@ import com.portfolio.proyecto.Entity.Persona;
 import com.portfolio.proyecto.Interface.IPersonaInterface;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaInterface iPersonaInterface;
     
@@ -46,6 +48,10 @@ public class PersonaController {
     
     iPersonaInterface.savePersona(persona);
     return persona;
+    }
+    @GetMapping("personas/traer/perfil")
+    public Persona findPersona(){
+    return iPersonaInterface.findPersona((long)1);
     }
     }
 
