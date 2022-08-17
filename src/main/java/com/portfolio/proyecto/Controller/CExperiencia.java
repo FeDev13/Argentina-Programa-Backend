bcacea46bd1960247a7ad35ca40e0a2ddce51550
USUARIO <USUARIO@DESKTOP-P3D2CGR.fibertel.com.ar>
2022-08-17 08:37:50 -0300

    fix CORS

--- src/main/java/com/portfolio/proyecto/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/proyecto/Controller/CExperiencia.java
@@ -26,7 +26,7 @@ import org.apache.commons.lang3.StringUtils;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfoliofedericorobledo.web.app/")
 public class CExperiencia {
   
     @Autowired
